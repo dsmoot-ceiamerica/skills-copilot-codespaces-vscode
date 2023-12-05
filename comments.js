07c6b7299b08c3943bba8015aa78d301396fdbe0
dsmoot-ceiamerica <152568140+dsmoot-ceiamerica@users.noreply.github.com>
2023-12-05 20:18:08 +0000

    xx

---
+++ comments.js
@@ -0,0 +1,71 @@
+// create web server using express
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+// data structure to store comments
+const commentsByPostId = {};
+
+// route to get comments by post id
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+// route to create new comment
+app.post('/posts/:id/comments', async (req, res) => {
+  const { content } = req.body;
+  const commentId = Math.random().toString(36).substr(2, 7);
+  const comments = commentsByPostId[req.params.id] || [];
+  comments.push({ id: commentId, content, status: 'pending' });
+  commentsByPostId[req.params.id] = comments;
+
+  // send event to event bus
+  await axios.post('http://event-bus-srv:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+      status: 'pending',
+    },
+  });
+
+  res.status(201).send(comments);
+});
+
+// route to receive events from event bus
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
+  console.log('Event Received:', type);
+
+  if (type === 'CommentModerated') {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+    comment.status = status;
+
+    // send event to event bus
+    await axios.post('http://event-bus-srv:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        status,
+        postId,
+        content,
+      },
+    });
+  }
+
+  res.send({});
+});
+
+app.listen(4001, () => {
+  console.log('Listening on 4001');
+});
